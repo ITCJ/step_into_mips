2bc3d6f5d455e8c6b74eeb8d6236101b9f00e6dc
糖橙结 <201901512330@email.sxu.edu.cn>
2022-06-13 18:19:24 +0800

    move ex段修改

--- openMips/ex.v
+++ openMips/ex.v
@@ -11,15 +11,118 @@ module ex(
     input [`RegAddrBus]     rw_i,
     input                   wreg_i,
 
+    // ---------- hilo 
+    input [`RegBus]         hi_i,
+    input [`RegBus]         lo_i,
+    input                   mem_whilo_i,
+    input [`RegBus]         mem_hi_i,
+    input [`RegBus]         mem_lo_i,
+    input                   wb_whilo_i,
+    input [`RegBus]         wb_hi_i,
+    input [`RegBus]         wb_lo_i,
+
+    // ---------- wreg
     output reg [`RegAddrBus]    rw_o,
     output reg                  wreg_o,
-    output reg [`RegBus]        wdata_o
+    output reg [`RegBus]        wdata_o,
+
+    // ---------- hilo 
+    output reg                   whilo_o,
+    output reg [`RegBus]         hi_o,
+    output reg [`RegBus]         lo_o
 );
 
 reg [`RegBus]   logicResult;
 reg [`RegBus]   shiftResult;
+reg [`RegBus]   moveResult;
+reg [`RegBus]   HI;
+reg [`RegBus]   LO;
 
-//---------- calculate
+//---------- HILO 冲突处理
+always @(*) begin
+    if (rst == `Enable) begin
+        HI <= `ZeroWord;
+        LO <= `ZeroWord;
+    end else if(mem_whilo_i == `Enable) begin
+        HI <= mem_hi_i;
+        LO <= mem_lo_i;
+    end else if(wb_whilo_i == `Enable) begin
+        HI <= wb_hi_i;
+        LO <= wb_lo_i;
+    end else begin
+        HI <= hi_i;
+        LO <= lo_i;
+    end
+end
+
+//----------move 数据通路
+always @(*) begin
+    if (rst == `Enable) begin
+        moveResult <= `ZeroWord;
+    end else begin
+        moveResult <= `ZeroWord;
+        case (aluop_i)
+            `EXE_MOVZ_OP: begin 
+                moveResult <= rdata1;
+            end
+
+            `EXE_MOVN_OP: begin 
+                moveResult <= rdata1;
+            end
+
+            `EXE_MFHI_OP: begin 
+                moveResult <= HI;
+            end
+
+            // `EXE_MTHI_OP: begin 
+            //     whilo_o <= `Enable;
+            //     hi_o <= rdata1;
+            //     lo_o <= LO;
+            // end
+
+            `EXE_MFLO_OP: begin 
+                moveResult <= LO;
+            end
+
+            // `EXE_MTLO_OP: begin 
+            //     whilo_o <= `Enable;
+            //     hi_o <= HI;
+            //     lo_o <= rdata1;
+            // end
+
+            default: begin
+                // whilo_o <= `Disable;
+                // hi_o    <= `ZeroWord;
+                // lo_o    <= `ZeroWord;
+            end
+        endcase
+
+    end
+end
+
+//---------- 独立另一mux
+
+always @ (*) begin
+    if(rst == `Enable) begin
+        whilo_o <= `Disable;
+        hi_o <= `ZeroWord;
+        lo_o <= `ZeroWord;		
+    end else if(aluop_i == `EXE_MTHI_OP) begin
+        whilo_o <= `Enable;
+        hi_o <= rdata1;
+        lo_o <= LO;
+    end else if(aluop_i == `EXE_MTLO_OP) begin
+        whilo_o <= `Enable;
+        hi_o <= HI;
+        lo_o <= rdata1;
+    end else begin
+        whilo_o <= `Disable;
+        hi_o <= `ZeroWord;
+        lo_o <= `ZeroWord;
+    end				
+end	
+
+//---------- calculate logical，shift 数据通路
 always @(*) begin
     if(rst == `Enable) begin
         logicResult <= `ZeroWord;
@@ -73,7 +176,7 @@ always @(*) begin
     end
 end
 
-//---------- result seletc
+//---------- result select
 always @(*) begin
     rw_o <= rw_i;
     wreg_o <= wreg_i;
@@ -85,6 +188,10 @@ always @(*) begin
 
         `EXE_RES_SHIFT: begin
             wdata_o <= shiftResult;
+        end
+
+        `EXE_RES_MOVE: begin
+            wdata_o <= moveResult;
         end
 
         default: begin
